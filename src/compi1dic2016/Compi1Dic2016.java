aa954f2b2e4ca2c3187e3c714e15aae1fa1520f7
daniel <daniel@alwud>
2017-01-08 11:34:37 -0600

    First Commit 0/

---
+++ src/compi1dic2016/Compi1Dic2016.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compi1dic2016;
+
+/**
+ *
+ * @author daniel
+ */
+public class Compi1Dic2016 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
